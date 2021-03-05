a095cf5fbe7db7efe886e5d55d9a6ebafae21bc6
Michael Pingel <mdpingel@gmail.com>
2021-03-05 12:36:41 -0800

    write GetOrderPrice_ReturnsOrderPrice test

--- PierreVendors.Tests/ModelTests/OrderTests.cs
+++ PierreVendors.Tests/ModelTests/OrderTests.cs
@@ -9,7 +9,7 @@ namespace PierreVendors.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Bread", "10 loaves of bread");
+      Order newOrder = new Order("Bread", "10 loaves of bread", 50);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
@@ -17,7 +17,7 @@ namespace PierreVendors.Tests
     public void GetOrderTitle_ReturnsOrderTitle_String()
     {
       string title = "Bread";
-      Order newOrder = new Order(title, "10 loaves of bread");
+      Order newOrder = new Order(title, "10 loaves of bread", 50);
       string result = newOrder.Title;
       Assert.AreEqual(title, result);
     }
@@ -26,9 +26,18 @@ namespace PierreVendors.Tests
     public void GetOrderDescription_ReturnsOrderDescription_String()
     {
       string description = "10 loaves of bread";
-      Order newOrder = new Order("Bread", description);
+      Order newOrder = new Order("Bread", description, 50);
       string result = newOrder.Description;
       Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void GetOrderPrice_ReturnsOrderPrice_Int()
+    {
+      int price = 50;
+      Order newOrder = new Order("Bread", "10 loaves of bread", price);
+      int result = newOrder.Price;
+      Assert.AreEqual(price, result);
     }
 
   }
