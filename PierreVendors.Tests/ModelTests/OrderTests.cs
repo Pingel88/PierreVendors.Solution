dcf1e826b7972cb75462f9a04685e62d092ef769
Michael Pingel <mdpingel@gmail.com>
2021-03-05 12:49:46 -0800

    remove redundant 'Order' from TestMethod names

--- PierreVendors.Tests/ModelTests/OrderTests.cs
+++ PierreVendors.Tests/ModelTests/OrderTests.cs
@@ -14,7 +14,7 @@ namespace PierreVendors.Tests
     }
 
     [TestMethod]
-    public void GetOrderTitle_ReturnsOrderTitle_String()
+    public void GetTitle_ReturnsTitle_String()
     {
       string title = "Bread";
       Order newOrder = new Order(title, "10 loaves of bread", 50, "03/05/2021");
@@ -23,7 +23,7 @@ namespace PierreVendors.Tests
     }
 
     [TestMethod]
-    public void GetOrderDescription_ReturnsOrderDescription_String()
+    public void GetDescription_ReturnsDescription_String()
     {
       string description = "10 loaves of bread";
       Order newOrder = new Order("Bread", description, 50, "03/05/2021");
@@ -32,7 +32,7 @@ namespace PierreVendors.Tests
     }
 
     [TestMethod]
-    public void GetOrderPrice_ReturnsOrderPrice_Int()
+    public void GetPrice_ReturnsPrice_Int()
     {
       int price = 50;
       Order newOrder = new Order("Bread", "10 loaves of bread", price, "03/05/2021");
@@ -41,7 +41,7 @@ namespace PierreVendors.Tests
     }
 
     [TestMethod]
-    public void GetOrderDate_ReturnsOrderDate_String()
+    public void GetDate_ReturnsDate_String()
     {
       string date = "03/05/2021";
       Order newOrder = new Order("Bread", "10 loaves of bread", 50, date);
