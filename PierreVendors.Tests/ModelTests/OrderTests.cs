a2eb0050f1971f6087a1d9930573a8e34b60980e
Michael Pingel <mdpingel@gmail.com>
2021-03-05 12:30:33 -0800

    write GetOrderDescription_ReturnsOrderDescription test

--- PierreVendors.Tests/ModelTests/OrderTests.cs
+++ PierreVendors.Tests/ModelTests/OrderTests.cs
@@ -9,7 +9,7 @@ namespace PierreVendors.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Bread");
+      Order newOrder = new Order("Bread", "10 loaves of bread");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
@@ -17,9 +17,18 @@ namespace PierreVendors.Tests
     public void GetOrderTitle_ReturnsOrderTitle_String()
     {
       string title = "Bread";
-      Order newOrder = new Order("Bread");
+      Order newOrder = new Order(title, "10 loaves of bread");
       string result = newOrder.Title;
       Assert.AreEqual(title, result);
+    }
+
+    [TestMethod]
+    public void GetOrderDescription_ReturnsOrderDescription_String()
+    {
+      string description = "10 loaves of bread";
+      Order newOrder = new Order("Bread", description);
+      string result = newOrder.Description;
+      Assert.AreEqual(description, result);
     }
 
   }
