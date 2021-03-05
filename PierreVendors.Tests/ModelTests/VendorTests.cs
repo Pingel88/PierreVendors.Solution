27904ec9e68662bc03a3ce345766f7c3f7ce8232
Michael Pingel <mdpingel@gmail.com>
2021-03-05 15:07:39 -0800

    write GetDescription_ReturnsDescription test

--- PierreVendors.Tests/ModelTests/VendorTests.cs
+++ PierreVendors.Tests/ModelTests/VendorTests.cs
@@ -9,7 +9,7 @@ namespace PierreVendors.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("Brad's Butter Emporium");
+      Vendor newVendor = new Vendor("Brad's Butter Emporium", "Typically buys a lot of bread to butter");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
@@ -17,9 +17,18 @@ namespace PierreVendors.Tests
     public void GetName_ReturnsName_String()
     {
       string name = "Brad's Butter Emporium";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "Typically buys a lot of bread to butter");
       string result = newVendor.Name;
       Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Typically buys a lot of bread to butter";
+      Vendor newVendor = new Vendor("Brad's Butter Emporium", description);
+      string result = newVendor.Description;
+      Assert.AreEqual(description, result);
     }
   }
 }
