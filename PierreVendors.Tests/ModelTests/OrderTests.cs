441351b176c95b1ffa1de92cb1b6235586fae61e
Michael Pingel <mdpingel@gmail.com>
2021-03-05 12:42:55 -0800

    write GetOrderDate_ReturnsOrderDate test

--- PierreVendors.Tests/ModelTests/OrderTests.cs
+++ PierreVendors.Tests/ModelTests/OrderTests.cs
@@ -9,7 +9,7 @@ namespace PierreVendors.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Bread", "10 loaves of bread", 50);
+      Order newOrder = new Order("Bread", "10 loaves of bread", 50, "03/05/2021");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
@@ -17,7 +17,7 @@ namespace PierreVendors.Tests
     public void GetOrderTitle_ReturnsOrderTitle_String()
     {
       string title = "Bread";
-      Order newOrder = new Order(title, "10 loaves of bread", 50);
+      Order newOrder = new Order(title, "10 loaves of bread", 50, "03/05/2021");
       string result = newOrder.Title;
       Assert.AreEqual(title, result);
     }
@@ -26,7 +26,7 @@ namespace PierreVendors.Tests
     public void GetOrderDescription_ReturnsOrderDescription_String()
     {
       string description = "10 loaves of bread";
-      Order newOrder = new Order("Bread", description, 50);
+      Order newOrder = new Order("Bread", description, 50, "03/05/2021");
       string result = newOrder.Description;
       Assert.AreEqual(description, result);
     }
@@ -35,9 +35,18 @@ namespace PierreVendors.Tests
     public void GetOrderPrice_ReturnsOrderPrice_Int()
     {
       int price = 50;
-      Order newOrder = new Order("Bread", "10 loaves of bread", price);
+      Order newOrder = new Order("Bread", "10 loaves of bread", price, "03/05/2021");
       int result = newOrder.Price;
       Assert.AreEqual(price, result);
+    }
+
+    [TestMethod]
+    public void GetOrderDate_ReturnsOrderDate_String()
+    {
+      string date = "03/05/2021";
+      Order newOrder = new Order("Bread", "10 loaves of bread", 50, date);
+      string result = newOrder.Date;
+      Assert.AreEqual(date, result);
     }
 
   }
